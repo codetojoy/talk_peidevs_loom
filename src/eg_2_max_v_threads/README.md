37cd191d3032ca7305b93d8f95fdd2e71447f044
Michael Easter <codetojoy@gmail.com>
2022-06-01 11:10:16 -0300

    examples 1, 2

---
+++ src/eg_2_max_v_threads/README.md
@@ -0,0 +1,31 @@
+
+Summary:
+---------
+
+* illustrates maxing out # of platform threads
+    - max on my Mac, default JVM is between 4k and 4.5k
+* Java 19 (preview) virtual threads
+
+To Build:
+---------
+
+* requires JDK 19.ea.24-open via [SDKMan!](https://sdkman.io/)
+* Gradle does not yet support JDK 19 preview (as of 01-JUN-2022). Check [here](https://docs.gradle.org/current/userguide/compatibility.html)
+* unknown if Maven can be used ¯\_(ツ)_/¯
+
+useful commands:
+
+* `sdk env`
+    - SDKMan! will set JDK to value in `.sdkmanrc`
+* `./clean.sh`
+* `./compile.sh`
+* `./run.sh`
+* note `./go.sh` is handy for clean-compile-run
+
+Flight Recorder:
+----------------
+
+* use `./run_with_flight_recorder.sh` to generate `flight.jfr`
+* install Java Mission Control 8.1.0 from [here](https://adoptopenjdk.net/jmc.html)
+* in terminal, run: `open JDK\ Mission\ Control.app/`
+* in app, open `flight.jfr`
---
+++ src/eg_2_max_v_threads/src/main/java/net/codetojoy/Runner.java
@@ -0,0 +1,31 @@
+
+// NOTE: I no longer own this domain
+package net.codetojoy;
+
+// from https://javahippie.net/java/concurrency/2022/04/12/getting-started-with-virtual-threads.html
+// and https://www.youtube.com/watch?v=UqlF6Mfhnz0
+
+public class Runner {
+    static void doWork() {
+        System.out.println("TRACER working t: " + Thread.currentThread());
+        try { Thread.sleep(5000); } catch (Exception ex) {}
+    }
+
+    void createManyThreads(int numThreads) throws Exception {
+        Thread thread = null;
+
+        for (int i = 0; i < numThreads; i++) {
+            // thread = new Thread(Runner::doWork);
+            thread = Thread.startVirtualThread(Runner::doWork);
+            // thread.start();
+        }
+
+        thread.join();
+    }
+
+    public static void main(String... args) throws Exception {
+        int numThreads = 20;
+        new Runner().createManyThreads(numThreads);
+        System.out.println("TRACER Ready v2");
+    }
+}
